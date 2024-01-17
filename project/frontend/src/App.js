c4fcacebabcd0cbb367c486e73af6944bf5983ea
LePygargue <46059303+LePygargue@users.noreply.github.com>
2024-01-17 13:43:44 +0100

    minor changes

--- project/frontend/src/App.js
+++ project/frontend/src/App.js
@@ -1,4 +1,5 @@
 // frontend/src/App.js
+import './App.css';
 import { React} from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Authentication from './features/authentication/index';
