b5d0045d01e149882a710c69525a7022a17cf410
LePygargue <46059303+LePygargue@users.noreply.github.com>
2024-01-17 10:55:53 +0100

    add registration with bcrypt hash and modify login to use bcrypt.compare

--- project/backend/server.js
+++ project/backend/server.js
@@ -2,18 +2,17 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const cors = require('cors'); // Importez le module cors
+const cors = require('cors');
 const usersRoute = require('./routes/users');
 const db = require('./db');
 
 const app = express();
 const port = 3001;
 
-app.use(cors()); // Utilisez le middleware cors
+app.use(cors());
 
 app.use(bodyParser.json());
 
-// Utilisation des routes
 app.use('/users', usersRoute);
 
 app.listen(port, () => {
--- project/frontend/src/App.js
+++ project/frontend/src/App.js
@@ -1,6 +1,7 @@
 // frontend/src/App.js
 import React from 'react';
 import Login from './components/Login';
+import Register from './components/Register';
 
 function App() {
   return (
